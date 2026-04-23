d030cec3e823bf7f2a2a6a8ea69f1267708a36ac
Aldrin Dsouza <oswaldbotpy@gmail.com>
2026-04-23 01:51:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import Home from './components/home';
-import './App.css'; // optional global styling
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return <Home />;
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-export default App;
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
